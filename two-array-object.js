f2a96de81758e4302cb20e986c3cdb1bdff754fb
Austin Chao <austinschao@gmail.com>
2022-03-11 16:21:00 -0800

    Completed two-array-objects

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,28 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+'use strict'
+
+/**Creating an object from an arr of keys
+ *  and arr of values
+*/
+
+
+// Paramater => arr of keys and arr of values
+// Return => An object made from the two arrays
+//           if the values is less than keys, add null
+
+// Init an empty object
+// iterate through the arr of keys and value
+//  at the same time and add them as a
+//  key value pair to the object
+
+
+function twoArrayObject(keys, values) {
+  const objFromArrays = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (values[i] === undefined) {
+      values[i] = null;
+    }
+    objFromArrays[keys[i]] = values[i];
+  }
+  return objFromArrays;
 }
